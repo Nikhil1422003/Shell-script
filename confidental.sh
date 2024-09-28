0b8226fef0639a2d2dfff8b34e01cd75341f3185
Nikhil <nikhil@123mail.com>
2024-09-28 19:35:49 +0530

    confidential

---
+++ confidental.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter Username::"
+
+read USERNAME #here username is variable
+
+echo "Please enter password::"
+
+read PASSWORD 
+
+echo "Username is : $USERNAME , Password is : $PASSWORD"
