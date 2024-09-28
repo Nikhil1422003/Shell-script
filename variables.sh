3af1706a951dc3fa5d7318ef696b42a0f29807a0
Nikhil <nikhil@123mail.com>
2024-09-28 19:05:21 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Suresh, how are you"
+echo "Suresh: Hi ramesh , iam fine what about you"
+echo "Ramesh: iam fine too hows your work"
+echo "Suresh: not bad , iam thinking to upgrade to devops"
