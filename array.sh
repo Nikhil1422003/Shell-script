d7f414a1534108d341594681ce2ff9abd2838b17
Nikhil <nikhil@123mail.com>
2024-09-28 19:54:39 +0530

    arrays

---
+++ array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIES =("Kalki2898AD" "Salaar" "Adipurush") 
+
+#size of the above array is 3
+#index are 0,1,2
+
+#list always starts with 0
+
+echo "first movie is  :${MOVIES[0]}"
+echo "second movie is :${MOVIES[1]}"
+echo "third movie is  :${MOVIES[2]}"
