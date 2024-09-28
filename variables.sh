7b41fb64d9f3975c142b35b757c39ae7394e61d8
Nikhil <nikhil@123mail.com>
2024-09-28 19:18:09 +0530

    vars

--- variables.sh
+++ variables.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+#declaring of variable
 PERSON1=Ramesh
 PERSON2=Suresh
 
+
+#refering of variable
 echo "$PERSON1: Hello $PERSON2, how are you"
 echo "$PERSON2: Hi $PERSON1 , iam fine what about you"
 echo "$PERSON1: iam fine too hows your work"
