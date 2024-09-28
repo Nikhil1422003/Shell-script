818683642323750c3d73ce79889ae222c9019384
Nikhil <nikhil@123mail.com>
2024-09-28 19:13:04 +0530

    vars

--- variables.sh
+++ variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh: Hello Suresh, how are you"
-echo "Suresh: Hi ramesh , iam fine what about you"
-echo "Ramesh: iam fine too hows your work"
-echo "Suresh: not bad , iam thinking to upgrade to devops"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hello $PERSON2, how are you"
+echo "$PERSON2: Hi $PERSON1 , iam fine what about you"
+echo "$PERSON1: iam fine too hows your work"
+echo "$PERSON2: not bad , iam thinking to upgrade to devops"
